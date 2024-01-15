a376cbe730b1709fc6dd82f3476d175636585880
Javezki <75654981+Javezki@users.noreply.github.com>
2024-01-15 16:24:16 -0500

    Final Chanages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
-import { useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 
 const FILTER_MAP = {
@@ -13,6 +13,15 @@ const FILTER_MAP = {
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
+
+  function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
+  
 
   const [filter, setFilter] = useState("All");
 
@@ -52,7 +61,7 @@ function App(props) {
   
 
   const taskList = tasks
-  .filter(FILTER_MAP[filter])
+  .filter(FILTER_MAP[filter]) 
   .map((task) => (
     <Todo 
       id={task.id} 
@@ -76,6 +85,18 @@ function App(props) {
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
+  const prevTaskLength = usePrevious(tasks.length);
+
+  const listHeadingRef = useRef(null);
+
+
+  useEffect(() => {
+    if (tasks.length < prevTaskLength) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+  
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -83,7 +104,9 @@ function App(props) {
       <div className="filters btn-group stack-exception">
       {filterList}
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1"ref={listHeadingRef}>
+        {headingText}
+      </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import {useEffect, useRef, useState } from "react";
+
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 
 function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
+
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
+
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -27,6 +43,7 @@ function Todo(props) {
           type="text"
           onChange={handleChange}
           value={newName}
+          ref={editFieldRef}
         />
       </div>
       <div className="btn-group">
@@ -64,7 +81,8 @@ function Todo(props) {
           className="btn"
           onClick={() => {
             setEditing(true);
-          }}>
+          }}
+          ref={editButtonRef}>
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button
@@ -76,6 +94,15 @@ function Todo(props) {
       </div>
     </div>
   );
+
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    } else if (wasEditing && !isEditing){
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]); 
+
 
   return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
